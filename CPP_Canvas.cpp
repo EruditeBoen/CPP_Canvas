a3d0e94d18c907fbfc8d306850fe39fe4351a3fb
EruditeBoen <boen.erudition@gmail.com>
2024-07-09 18:09:53 -0400

    Canvas

--- CPP_Canvas.cpp
+++ CPP_Canvas.cpp
@@ -10,3 +10,27 @@
 
 using namespace std;
 
+class Solution {
+	// Solution function here :>
+
+	// Solution function end :O
+};
+
+int main() {
+	Solution sol;
+
+	// Test case variables here :v
+
+	// Test case variable end :<
+
+	int ans = sol.// Solution
+
+	// Uncomment for vector output
+	// for {auto item : ans}
+	// 	cout << item << " " << endl;
+
+	// Uncomment for single output
+	// cout << ans << endl;
+
+	return 0;
+}
